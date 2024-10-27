bffc01f7062f1669f57847fed1ae77b326d9a5a2
Thomas Hirsch <84n4n4@gmail.com>
2024-10-27 13:31:20 +0100

    more notebooks and assignment 2

---
+++ src/exercise_2/bonus_exercise_2.py
@@ -0,0 +1,23 @@
+# Exercise 2 - bonus.
+
+import numpy as np
+
+
+# Todo - use monte carlo method to claculate pi, see https://en.wikipedia.org/wiki/Monte_Carlo_method
+#   Hint: Use numpy to generate a random numbers.
+#   Hint: Easiest would be to have them between 0 and 1.
+#   Hint: Two random numbers form a point in 2d.
+#   Hint: Calculate the distance of each random point from the source (0,0)
+#   Hint: Points that have a distance of <= 1 would lie within a circle of radius 1.
+#   Hint: Points that are further away are outside of the such a circle.
+#   Hint: Count how many points end up in either group. The relation of this point counts can be used to calculate pi.
+def monte_carlo_pi():
+    return None
+
+
+def main():
+    monte_carlo_pi()
+
+
+if __name__ == '__main__':
+    main()
